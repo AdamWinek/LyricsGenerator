9412041705d1bb3875e80d4ddcf5ba46cb8b8b33
Adam Winek <adamwinek@gmail.com>
2020-03-18 00:01:33 -0400

    hello

---
+++ Maibn.py
@@ -0,0 +1,40 @@
+import bs4 as BeautifulSoup
+import urllib.request
+
+
+sourcePage = "https://www.metrolyrics.com/drake-alpage-"
+sourcePageEnding = ".html"
+pages = 7
+exportfile = open("g=drakeLyrics.txt","w+")
+
+
+
+def getAllLinks(numOfPages):
+    linklist = list()
+    for i in range (0, numOfPages + 1):
+        source = urllib.request.urlopen(sourcePage + str(i) + sourcePageEnding)
+        soup = BeautifulSoup.BeautifulSoup(source, "html.parser")
+        links = soup.find_all("a", attrs={"class":"title hasvidtable"})
+
+        for item in links:
+            x = item.get('href', None)
+            if x !="https://www.metrolyrics.com/sicko-mode-lyrics-travis-scott.html"  and x != "https://www.metrolyrics.com/work-lyrics-rihanna.html" and x != "https://www.metrolyrics.com/yes-indeed-lyrics-lil-baby.html" and x != "https://www.metrolyrics.com/bedrock-lyrics-lil-wayne.html" and x != "https://www.metrolyrics.com/whats-my-name-lyrics-rihanna.html" and x != "https://www.metrolyrics.com/only-lyrics-nicki-minaj.html" and x != "https://www.metrolyrics.com/unthinkable-lyrics-alicia-keys.html" and x != "https://www.metrolyrics.com/moment-for-life-lyrics-nicki-minaj.html" and x != "https://www.metrolyrics.com/walk-it-talk-it-lyrics-migos.html" and x != "https://www.metrolyrics.com/im-on-one-lyrics-dj-khaled.html" and x != "https://www.metrolyrics.com/come-and-see-me-lyrics-party-next-door.html" and x != "https://www.metrolyrics.com/moment-4-life-lyrics-nicki-minaj.html" and x != "https://www.metrolyrics.com/truffle-butter-lyrics-nicki-minaj.html" and x != "https://www.metrolyrics.com/im-goin-in-lyrics-lil-wayne.html":
+                linklist.append(x)
+    return linklist
+
+def songPageToLyrics(website):
+    source = urllib.request.urlopen(website)
+    soup = BeautifulSoup.BeautifulSoup(source, 'html.parser')
+
+    lyrics = soup.find_all("p", attrs={"class": "verse"})
+    lyricsText = ""
+    for i in lyrics:
+         current = i.text.strip()
+         lyricsText +=  current
+
+    exportfile.write(lyricsText)
+
+
+for current_link in getAllLinks(7):
+    songPageToLyrics(current_link)
+exportfile.close()
