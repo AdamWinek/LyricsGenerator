70b9feb0cb464dd369979902c4dc35da28553bca
Adam Winek <adamwinek@gmail.com>
2020-04-11 23:58:29 -0400

    connected wires

--- LyricsGeneratorFlask/static/js/sendRequest.js
+++ LyricsGeneratorFlask/static/js/sendRequest.js
@@ -1,13 +1,24 @@
-function sendRequest(){
 
+window.onload=function(){
+  document.getElementById("button").addEventListener("click", sendRequest)
 
+}
+
+function sendRequest(e){
+  console.log(e)
+  e.preventDefault()
   data = fetch('/generate')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    console.log(data);
+    document.getElementById("returnText").textContent = "";
+    lyricsText = data["lyrics"];
+    console.log(lyricsText)
+    document.getElementById("returnText").textContent += lyricsText;
     
   });
-  document.getElementById("buttonResponse").textContent = data;
+  
+  
+  
 }
